666cf92250ab6e1551d3a8e148f58d1193ab8017
prashant169 <prashantphad2018@gmail.com>
2021-10-20 06:18:12 -0800

    Added:Intialising Deck of cards and Printing them

---
+++ WorkshopDackOfCards/src/com/bridgelabz/DecksOfCards.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class DecksOfCards {
+	public static void main(String[] args) {
+		System.out.println("Welcome into the Decks of Cards");
+		
+		//Create the Object Of CardPlay
+		CardsPlay cardsPlay = new CardsPlay();
+		
+		//Accessing  method
+		cardsPlay.generateCards();
+	}
+}
